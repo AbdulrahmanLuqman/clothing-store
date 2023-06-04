c3985e58af3d19337f3b1a59eb94e654b347e10c
AbdulrahmanLuqman <fal528@dervac.com>
2023-06-04 22:02:45 +0100

    imported carousel to landingPage

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,5 +1,6 @@
 <script>
 import { defineComponent } from 'vue';
+import VueCarousel from 'vue-carousel';
 
 export default defineComponent({
     data(){
@@ -10,46 +11,9 @@ export default defineComponent({
 })
 </script>
 <template>
-    <div class="header">
-        <div class="first-bg">
 
-        </div>
-        <div class="sec-bg">
-
-        </div>
-        <div class="third-bg">
-
-        </div>
-    </div>
 </template>
 
 <style scoped>
 
-    .header{
-        
-    }
-    .first-bg{
-        background-image: url(../assets/cloth.jpg);
-        height: 100vh;
-        width: 100%;
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: right;
-    }
-    .sec-bg{
-        background-image: url(../assets/ring.jpg);
-        height: 100vh;
-        width: 100%;
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: right;
-    }
-    .third-bg{
-        background-image: url(../assets/elect.jpg);
-        height: 100vh;
-        width: 100%;
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: right;
-    }
 </style>
