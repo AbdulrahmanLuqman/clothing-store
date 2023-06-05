0006f49617482392af780538ba7880935aa96c3c
AbdulrahmanLuqman <fal528@dervac.com>
2023-06-05 08:30:39 +0100

    added title attributes to navs

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,7 +29,7 @@
                 <p class="text-[17px] font-semibold hover:bg-[#B86EE7] hover:text-white h-fit py-[19px] w-fit px-10  max-[860px]:w-full cursor-pointer" @click="slideOut = !slideOut" title="accessories"><RouterLink to="/accessories">accessories</RouterLink></p>
                 <p class="text-[17px] font-semibold hover:bg-[#B86EE7] hover:text-white h-fit py-[19px] w-fit px-10  max-[860px]:w-full cursor-pointer" @click="slideOut = !slideOut" title="electronics"><RouterLink to="/electronics">electronics</RouterLink></p>
             </nav>
-            <div class="cursor-pointer">
+            <div class="cursor-pointer" title="cart">
                 <RouterLink to="/cart"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#B86EE7" d="M7 22q-.825 0-1.413-.588T5 20q0-.825.588-1.413T7 18q.825 0 1.413.588T9 20q0 .825-.588 1.413T7 22Zm10 0q-.825 0-1.413-.588T15 20q0-.825.588-1.413T17 18q.825 0 1.413.588T19 20q0 .825-.588 1.413T17 22ZM5.2 4h14.75q.575 0 .875.513t.025 1.037l-3.55 6.4q-.275.5-.738.775T15.55 13H8.1L7 15h12v2H7q-1.125 0-1.7-.988t-.05-1.962L6.6 11.6L3 4H1V2h3.25l.95 2Z"/></svg></RouterLink>
             </div>
         </div>
