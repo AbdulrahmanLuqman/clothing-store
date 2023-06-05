86a3074a53e8cd02aafbd494a38a678e9bf63b0d
AbdulrahmanLuqman <fal528@dervac.com>
2023-06-05 11:50:16 +0100

    added z-index

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -12,26 +12,32 @@ export default defineComponent({
 </script>
 <template>
    <div class="cont flex w-[300%] z-[-99]">
-        <div class="first-bg">
-            <div class="h-full flex flex-col gap-6 justify-center lg:pl-[100px] lg:w-[50%] pl-10">
+        <div class="first-bg relative">
+            <div class="h-full flex flex-col gap-6 justify-center lg:pl-[100px] lg:w-[50%] pl-10 z-50 absolute">
                 <p class="lg:text-2xl text-[#B86EE7] ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, atque.</p>
                 <p class="text-base text-white font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint eaque ullam optio voluptatum qui incidunt debitis assumenda, officia iste hic ex animi, autem, fugiat est veritatis quidem rem earum modi!</p>
                 <p class="w-fit h-fit bg-[#B86EE7] py-2 px-6 rounded-lg font-bold"><RouterLink to="/clothing">view clothing</RouterLink></p>   
             </div>
+
+            <div class="absolute top-0 bg-black h-full w-full opacity-50 z-10"></div>
         </div>
-        <div class="sec-bg">
-            <div class="h-full flex flex-col gap-6 justify-center lg:pl-[100px] lg:w-[50%] pl-10">
+        <div class="sec-bg relative">
+            <div class="h-full flex flex-col gap-6 justify-center lg:pl-[100px] lg:w-[50%] pl-10 z-50 absolute">
                 <p class="lg:text-2xl text-[#B86EE7] ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, atque.</p>
                 <p class="text-base text-white font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint eaque ullam optio voluptatum qui incidunt debitis assumenda, officia iste hic ex animi, autem, fugiat est veritatis quidem rem earum modi!</p>
                 <p class="w-fit h-fit bg-[#B86EE7] py-2 px-6 rounded-lg font-bold"><RouterLink to="/accessories">view accessories</RouterLink></p>   
             </div>
+            <div class="absolute top-0 bg-black h-full w-full opacity-50 z-10"></div>
+
         </div>
-        <div class="third-bg">
-            <div class="h-full flex flex-col gap-6 justify-center lg:pl-[100px] lg:w-[50%] pl-10">
+        <div class="third-bg relative">
+            <div class="h-full flex flex-col gap-6 justify-center lg:pl-[100px] lg:w-[50%] pl-10 z-50 absolute">
                 <p class="lg:text-2xl text-[#B86EE7] ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, atque.</p>
                 <p class="text-base text-white font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint eaque ullam optio voluptatum qui incidunt debitis assumenda, officia iste hic ex animi, autem, fugiat est veritatis quidem rem earum modi!</p>
                 <p class="w-fit h-fit bg-[#B86EE7] py-2 px-6 rounded-lg font-bold"><RouterLink to="/electronics">view electronics</RouterLink></p>   
             </div>
+            <div class="absolute top-0 bg-black h-full w-full opacity-50 z-10"></div>
+
         </div>
     </div>
 </template>
@@ -64,6 +70,7 @@ export default defineComponent({
     .cont{
         animation: slide 15s infinite 2s alternate ease;
         position: relative;
+        overflow: hidden;
     }
     @keyframes slide {
         0%{
